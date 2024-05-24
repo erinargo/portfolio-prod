4030bccb374904c05126387fa0d26091231b7e6e
Erin Argo <d_argo@uncg.edu>
2024-05-24 13:08:57 -0400

    https integration

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 
 const bodyparser = require('body-parser');
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
 
-const PORT = process.env.port || 80;
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+const app = express();
+const httpServer = http.createServer(app);
+const httpsServer = http.createServer(credentials, app);
+
+const HTTP_PORT = process.env.port || 80;
+const HTTPS_PORT = process.env.port || 443;
 
 app.use(bodyparser.json()); // Use middleware to interpret JSON
 app.use(express.static(__dirname + '/front-end/build')); // Express will only read from front-end/build
@@ -17,4 +27,5 @@ app.get('*', (req, res) => { // App will accept all paths (ie /login /register /
     res.sendFile(path.join(__dirname + '/front-end/build/index.html')); // Path middleware to make life easier
 });
 
-server.listen(PORT, () => console.log('Listening on: ' + PORT));
+httpServer.listen(HTTP_PORT, () => console.log('Listening on: ' + HTTP_PORT));
+httpsServer.listen(HTTPS_PORT, () => console.log('Listening on: ' + HTTPS_PORT));
